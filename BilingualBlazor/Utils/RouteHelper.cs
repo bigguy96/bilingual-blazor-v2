f1a690fa1f7d37171b17b94b4b8c9ccee5c79d23
GT <gaetan23@msn.com>
2020-10-25 13:33:32 -0400

    code refactor.

--- BilingualBlazor/Utils/RouteHelper.cs
+++ BilingualBlazor/Utils/RouteHelper.cs
@@ -12,36 +12,30 @@ namespace BilingualBlazor.Utils
             if (path.Equals("/")) return path;
 
             var alternate = twoLetterRequestCulture.Equals("en", StringComparison.CurrentCultureIgnoreCase) ? "fr" : "en";
-            var split = path.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            var splitPath = path.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
 
-            if (split.Length == 1)
-            {
-                return $"/{alternate}";
-            }
+            if (splitPath.Length < 2) return $"/{alternate}";
 
-            if (split.Length >= 2)
-            {
-                path = $"{split[0]}/{split[1]}";
-            }
+            var current = $"/{splitPath[0]}/{splitPath[1]}";
 
             // Get all the components whose base class is ComponentBase
             // Option 1
             var components = Assembly.GetExecutingAssembly().ExportedTypes.Where(t => t.IsSubclassOf(typeof(ComponentBase))).ToList();
             var routables = components.Where(c => c.GetCustomAttributes(inherit: true).OfType<RouteAttribute>().ToArray().Any());
             var alternateRoute = routables.Select(s => new { RouteAttributes = s.GetCustomAttributes(inherit: true).OfType<RouteAttribute>() })
-                .SingleOrDefault(x => x.RouteAttributes.Any(a => a.Template.Contains(path)))?.RouteAttributes
+                .SingleOrDefault(x => x.RouteAttributes.Any(a => a.Template.Contains(current)))?.RouteAttributes
                 .SingleOrDefault(s => s.Template.Contains(alternate))?.Template;
 
             if (alternateRoute == null) return $"/{alternate}";
 
             var alternateRouteSplit = alternateRoute.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
-            var newRoute = $"/{alternateRouteSplit[0]}/{alternateRouteSplit[1]}";
-            for (var i = 2; i < split.Length; i++)
+            var alternatePath = $"/{alternateRouteSplit[0]}/{alternateRouteSplit[1]}";
+            for (var i = 2; i < splitPath.Length; i++)
             {
-                newRoute += $"/{split[i]}"; ;
+                alternatePath += $"/{splitPath[i]}"; ;
             }
 
-            return newRoute;
+            return alternatePath;
         }
     }
 }
