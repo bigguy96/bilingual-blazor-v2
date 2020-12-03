f832e061c629dd360d84c2c22c05b322c517289e
GT <26967905+bigguy96@users.noreply.github.com>
2020-12-03 16:31:19 -0500

    fix on alternate route.

--- BilingualBlazor/Utils/RouteHelper.cs
+++ BilingualBlazor/Utils/RouteHelper.cs
@@ -11,10 +11,10 @@ namespace BilingualBlazor.Utils
         {
             if (path.Equals("/")) return path;
 
-            var alternate = twoLetterRequestCulture.Equals("en", StringComparison.CurrentCultureIgnoreCase) ? "fr" : "en";
+            var alternate = twoLetterRequestCulture.Equals("en", StringComparison.CurrentCultureIgnoreCase) ? "/fr/" : "/en/";
             var splitPath = path.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
 
-            if (splitPath.Length < 2) return $"/{alternate}";
+            if (splitPath.Length < 2) return alternate;
 
             var current = $"/{splitPath[0]}/{splitPath[1]}";
 
@@ -22,11 +22,16 @@ namespace BilingualBlazor.Utils
             // Option 1
             var components = Assembly.GetExecutingAssembly().ExportedTypes.Where(t => t.IsSubclassOf(typeof(ComponentBase))).ToList();
             var routables = components.Where(c => c.GetCustomAttributes(inherit: true).OfType<RouteAttribute>().ToArray().Any());
+
+            //var t = routables.Select(s => new
+            //        {RouteAttributes = s.GetCustomAttributes(inherit: true).OfType<RouteAttribute>()})
+            //    .SingleOrDefault(x => x.RouteAttributes.Any(a => a.Template.Contains(current)));
+
             var alternateRoute = routables.Select(s => new { RouteAttributes = s.GetCustomAttributes(inherit: true).OfType<RouteAttribute>() })
                 .SingleOrDefault(x => x.RouteAttributes.Any(a => a.Template.Contains(current)))?.RouteAttributes
                 .SingleOrDefault(s => s.Template.Contains(alternate))?.Template;
 
-            if (alternateRoute == null) return $"/{alternate}";
+            if (alternateRoute == null) return alternate;
 
             var alternateRouteSplit = alternateRoute.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
             var alternatePath = $"/{alternateRouteSplit[0]}/{alternateRouteSplit[1]}";
