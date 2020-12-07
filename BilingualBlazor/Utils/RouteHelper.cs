ae0f638fe7ce81e07f525358e5a85cdda51e5cec
GT <26967905+bigguy96@users.noreply.github.com>
2020-12-06 22:52:10 -0500

    code cleanup and a few comments.

--- BilingualBlazor/Utils/RouteHelper.cs
+++ BilingualBlazor/Utils/RouteHelper.cs
@@ -18,21 +18,18 @@ namespace BilingualBlazor.Utils
 
             var current = $"/{splitPath[0]}/{splitPath[1]}";
 
-            // Get all the components whose base class is ComponentBase
-            // Option 1
+            // Get all the components whose base class is ComponentBase.
             var components = Assembly.GetExecutingAssembly().ExportedTypes.Where(t => t.IsSubclassOf(typeof(ComponentBase))).ToList();
-            var routables = components.Where(c => c.GetCustomAttributes(inherit: true).OfType<RouteAttribute>().ToArray().Any());
+            var routingTables = components.Where(c => c.GetCustomAttributes(inherit: true).OfType<RouteAttribute>().ToArray().Any());
 
-            //var t = routables.Select(s => new
-            //        {RouteAttributes = s.GetCustomAttributes(inherit: true).OfType<RouteAttribute>()})
-            //    .SingleOrDefault(x => x.RouteAttributes.Any(a => a.Template.Contains(current)));
-
-            var alternateRoute = routables.Select(s => new { RouteAttributes = s.GetCustomAttributes(inherit: true).OfType<RouteAttribute>() })
+            //Get alternate routes (english to french and vice versa).
+            var alternateRoute = routingTables.Select(s => new { RouteAttributes = s.GetCustomAttributes(inherit: true).OfType<RouteAttribute>() })
                 .SingleOrDefault(x => x.RouteAttributes.Any(a => a.Template.Contains(current)))?.RouteAttributes
                 .SingleOrDefault(s => s.Template.Contains(alternate))?.Template;
 
             if (alternateRoute == null) return alternate;
 
+            //Rebuild complete alternate path.
             var alternateRouteSplit = alternateRoute.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
             var alternatePath = $"/{alternateRouteSplit[0]}/{alternateRouteSplit[1]}";
             for (var i = 2; i < splitPath.Length; i++)
