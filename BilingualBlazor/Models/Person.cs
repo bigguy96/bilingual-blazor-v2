afd852fcd1a973ed533190cb672751c242433a67
GT <26967905+bigguy96@users.noreply.github.com>
2020-12-28 17:06:49 -0500

    Added sample form.

---
+++ BilingualBlazor/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BilingualBlazor.Models
+{
+    public class Person
+    {
+        [Required]
+        public bool IsHuman { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Range(1, 99, ErrorMessage = "Age must be between 1 and 99.")]
+        public int Age { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male, Female, Other
+    }
+}
