cae5fe06c1480359d7c6fe44de092da7d9b07051
GT <26967905+bigguy96@users.noreply.github.com>
2021-03-14 13:12:01 -0400

    Added navigation service.

---
+++ BilingualBlazor/NavigationService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BilingualBlazor
+{
+    public class NavigationService
+    {
+        protected NavigationManager NavigationManager { get; }
+
+        public NavigationService(NavigationManager navigationManager)
+        {
+            NavigationManager = navigationManager;
+        }
+
+        public void GoTo(string url)
+        {
+            NavigationManager.NavigateTo(url, true);
+        }
+    }
+}
+
+//https://stackoverflow.com/questions/61082293/can-i-call-navigationmanager-in-a-blazor-service
